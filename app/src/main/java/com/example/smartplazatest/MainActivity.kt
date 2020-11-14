1fcb412d3dfdae4d1fb5f64276c207fe8301968d
orken <kyrgaulorken@gmail.com>
2020-11-14 17:45:06 +0600

    cutom toolbar

--- app/src/main/java/com/example/smartplazatest/MainActivity.kt
+++ app/src/main/java/com/example/smartplazatest/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        actionBar?.hide()
         bottomNavigationView = findViewById(R.id.chipNav)
         if(savedInstanceState == null) {
             handleFrame(HomeFragment())
