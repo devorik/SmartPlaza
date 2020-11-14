4916e3c7189535dd6b4999c46c9bbccb1d671308
orken <kyrgaulorken@gmail.com>
2020-11-14 15:54:01 +0600

    Switching between fragments

--- app/src/main/java/com/example/smartplazatest/MainActivity.kt
+++ app/src/main/java/com/example/smartplazatest/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.smartplazatest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.smartplazatest.ui.HomeFragment
+import com.example.smartplazatest.ui.WalletFragment
+import com.ismaeldivita.chipnavigation.ChipNavigationBar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView : ChipNavigationBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.chipNav)
+        if(savedInstanceState == null) {
+            handleFrame(HomeFragment())
+        }
+        bottomNavigationView.setItemSelected(R.id.home,true)
+        bottomNavigationView.setOnItemSelectedListener {id ->
+            when(id) {
+                R.id.home -> handleFrame(HomeFragment())
+                R.id.wallet -> handleFrame(WalletFragment())
+            }
+        }
     }
+
+    private fun handleFrame(fragment: Fragment):Boolean {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_to_fragments,fragment).commit()
+        return true
+    }
+
 }
